8181ca76b0604b407e57a7dfc25c9d1f358fd4bd
Dimitris Elef8eriou <Dimitris Elef8eriou>
2016-01-27 15:42:03 +0000

    first commit

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, url
+from rango import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    # Examples:
+    # url(r'^$', 'tango_with_django_project_17.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^about/',views.about,name='about'),)
+   
